636daf431e850f4677a05c3371dab940670df44c
cnzmg <cnzmg@git.com>
2020-03-12 18:17:15 +0800

    new project assurances

---
+++ assurance/gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, parallel, watch, series } = require('gulp');
+const pug = require('gulp-pug');
+const less = require('gulp-less');
+const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
+
+function html() {
+  return src('client/templates/*.pug')
+    .pipe(pug())
+    .pipe(dest('build/html'))
+}
+
+function css() {
+  return src('client/templates/*.less')
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(dest('build/css'))
+}
+
+function js() {
+  return src('client/javascript/*.js', { sourcemaps: true })
+    .pipe(concat('app.min.js'))
+    .pipe(dest('build/js', { sourcemaps: true }))
+}
+
+watch('client/', series(html, css, js));
+
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.default = parallel(html, css, js);
---
+++ assurances/templates/javascripts/index.js
@@ -0,0 +1,170 @@
+import '../stylesheets/style.min.css';
+
+class init{
+    constructor(){
+        this.data = {};
+        document.querySelectorAll('input').forEach((element, index) => {
+            this.data[element.name] = element.value;
+        })
+        this.dom = document.querySelector('.submit');
+        this._submit_ = this._submit_(location.href.substring(location.href.lastIndexOf('/') + 1, location.href.length - 5));
+    }
+
+    _submit_(params){
+        // document.querySelector('.module').addEventListener('click', this._clone_());
+        console.log(document.querySelector('.module'));
+        switch(params){
+            case 'index':
+                this.dom.onclick = domain => {
+                    
+                };
+                document.querySelectorAll('.pullimage').forEach((element, index) => {
+                    document.querySelectorAll('.pullimage')[index].onclick = domain => {
+                        this._image_(element.name, index);  //触发图片上传
+                    }
+                })
+                this._enlarge_(); //注册示意图放大事件
+                break;
+            case 'login':
+                console.log(123)
+                break;
+            default:
+                break;
+        }
+    }
+
+    _enlarge_(params){  //放大图片
+        document.querySelectorAll('.sketchmap').forEach((element, index) => {
+            element.onclick = domain => {
+                this._show_('.image-show');
+                let srcurl = document.querySelectorAll('.sketchmap>img')[index].getAttribute('src');
+                document.querySelectorAll('.image-show>img')[0].setAttribute('src', srcurl);
+            }
+        })
+    }
+
+    _show_(className){  //显示方法
+        document.querySelector('.module').style.display = 'block';
+        document.querySelectorAll(className).forEach((element, index) => {
+            document.querySelectorAll(className)[index].style.display = 'block';
+        });
+    }
+
+    _clone_(className){  //隐藏方法
+        document.querySelector('.module').style.display = 'none';
+        document.querySelectorAll(className).forEach((element, index) => { 
+            document.querySelectorAll(className)[index].style.display = 'none';
+        });
+    }
+
+    _image_(name, index){
+        document.getElementsByClassName(name)[0].click();
+        document.getElementsByClassName(name)[0].onchange = function (e) {
+            var localFile = this.files[0];
+            var reader = new FileReader();
+            var content;
+            reader.onload = function (event) {
+                content = event.target.result;
+
+                //暂定
+                document.querySelectorAll('.pullimage')[index].setAttribute('src', content);
+
+                compress(content, 450, function (contentFile) {
+                    // push image
+                    // let _$file = new FormData();
+                    // _$file.append('maintainerId', assign.maintainerId);
+                    // _$file.append('type', 18);
+                    // _$file.append('file', contentFile, 'machineNumber_' + Math.random() + '.png');
+                    // axios({
+                    //     method: "POST",
+                    //     url: filePush,
+                    //     data: _$file,
+                    //     processData: false,
+                    //     traditional: true,
+                    //     contentType: false,
+                    //     headers: {
+                    //         "Content-Type": false
+                    //     },
+                    //     onUploadProgress: function (progressEvent) { //原生获取上传进度的事件
+                    //         if (progressEvent.lengthComputable) {
+                    //             //属性lengthComputable主要表明总共需要完成的工作量和已经完成的工作是否可以被测量
+                    //             //如果lengthComputable为false，就获取不到progressEvent.total和progressEvent.loaded
+                    //             if (progressEvent.total % progressEvent.loaded == +false) {
+                    //                 setTimeout(() => {
+                    //                     loading.style.display = 'none';
+                    //                 }, 2000)
+                    //             }
+                    //         }
+                    //     }
+                    // }).then(
+                    //     response => {
+                            // let _imgBox = document.createElement('figure'), _img = document.createElement('img'), _clone = document.createElement('svg'), _use = document.createElement('use');
+                            // _imgBox.className = 'hash[imageBox]';
+                            // _img.src = response.data.realPath;
+                            // _imgBox.appendChild(_img);
+                            // _clone.className = 'icon';
+                            // _clone.setAttribute('aria-hidden', "true");
+                            // _use.setAttribute('xlink:href', "#ym-icon-guanbi");
+                            // _clone.appendChild(_use);
+                            // _imgBox.appendChild(_clone);
+                            // photo.appendChild(_imgBox);
+                    //     }
+                    // ).catch((error) => {
+                    //     console.log(error);
+                    // })
+                });
+            };
+            reader.onerror = function () {
+                alert("error");
+            };
+            reader.readAsDataURL(localFile, "UTF-8");
+        }
+        function compress(content, size, callback) {  //压缩拍摄上传
+            if (content.length <= size * 1024) {
+                callback(dataURItoBlob(content));
+                return;
+            }
+            let canvas = document.createElement("canvas");
+            let ctx = canvas.getContext("2d");
+            let img = new Image();
+            img.src = content;
+            img.onload = function () {
+                let width = img.width;
+                let height = img.height;
+                canvas.width = width;
+                canvas.height = height;
+                // 铺底色
+                ctx.fillStyle = "#fff";
+                ctx.fillRect(0, 0, width, height);
+                ctx.drawImage(img, 0, 0, width, height);
+                let rate = (1024 * size) / content.length;
+                console.log(content.length * 1024);
+                //进行压缩
+                content = canvas.toDataURL("image/jpeg", 0.2);
+                console.log(content.length * 1024);
+                let blob = dataURItoBlob(content);
+                callback(blob);
+            };
+        }
+        /**
+         * base64 转二进制文件
+         * @param {*} base64Data 
+         */
+        function dataURItoBlob(base64Data) {
+            var bytes = window.atob(base64Data.split(',')[1]); //去掉url的头，并转换为byte
+
+            //处理异常,将ascii码小于0的转换为大于0
+            var ab = new ArrayBuffer(bytes.length);
+            var ia = new Uint8Array(ab);
+            for (var i = 0; i < bytes.length; i++) {
+                ia[i] = bytes.charCodeAt(i);
+            }
+            return new Blob([ab], {
+                type: 'image/png'
+            });
+        }
+
+    }
+}
+
+window.addEventListener('DOMContentLoaded', new init());
